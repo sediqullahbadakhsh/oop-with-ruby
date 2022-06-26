9a482ec1f3e8c6893a80eadeb8f8d40ae4475abc
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-27 01:02:15 +0430

    practice control structures

--- Ruby-Basics/control-structures.rb
+++ Ruby-Basics/control-structures.rb
@@ -1,16 +1,56 @@
 # Control Structures in Ruby
-# including (if, While) without braces
 
-# if Statment example
+# if...else Statment example, Syntax:
+=begin
+    if condition [then]
+        code...
+    [elsif condition [then]
+        code... ]
+    [else
+        code...]
+    end
+=end
 
-    if number > 10
-        puts "You Win"   
-    else
-        puts 'You Lose'
-    end  
+# Ruby if Modifier, Syntax
+# code if condition
 
-ifControl(3)
-ifControl(11)
+# Ruby unless statement, Syntax:
+# executes code if condition is false, if ture the else clause will be executed
+
+=begin
+    unless conditional [then]
+        code...
+    [else
+        code...]
+    end
+=end
+
+
+# Ruby Unless modifier
+# code unless conditional
+
+# Ruby Case Statement, Syntax
+=begin
+case expression
+    [when] expression [, Expression...] then
+        code]...
+    [else
+        code...]
+    end
+=end
+$age =  5
+case $age
+when 0 .. 2
+   puts "baby"
+when 3 .. 6
+   puts "little child"
+when 7 .. 12
+   puts "child"
+when 13 .. 18
+   puts "youth"
+else
+   puts "adult"
+end
 
 # While Statement example
 while weight < 100 and numPallets <=30
---
+++ Ruby-Basics/ruby-operators.rb
@@ -0,0 +1,23 @@
+# Ruby Arithmetic Operators
+# { +, -, *, /, %, **(exponent"power")}
+
+# Ruby Comparison Operators
+=begin
+    == Equal Operator
+    != Not Equal
+    > Greater than
+    < Less than
+    >= Greater or equal
+    <= Less than or equal
+    <=> Combined comparison Operator.return 0 if first operand equal second,
+    1 if first operand is greater than the second and -1 if first operand is less than the second
+    === test equality e.g. (1...10) === 5 returns true
+    .eql? true if the receiver and argument have both the same type and equla values
+    equal? true if the receiver and argument have the same object id.
+       
+end
+
+# Ruby Bitwise Operator
+# {&(AND), |(OR), ^XOR, <<(Binary left shift operator), >>(Binary right shift operator)}
+
+# Ruby Dot"." and Double Colon"::"
